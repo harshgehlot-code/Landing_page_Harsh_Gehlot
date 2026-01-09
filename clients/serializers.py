bff6415318f5932b400746b4be1b60aa57510369
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2026-01-09 15:21:37 +0530

    add clients files

---
+++ clients/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Client
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'image', 'name', 'description', 'designation', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+
