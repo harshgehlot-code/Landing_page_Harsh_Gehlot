24ea0597fe4f50fee9e9f658e98b984c7446ec7a
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2026-01-10 23:00:35 +0530

    update clients/models.py files

--- clients/models.py
+++ clients/models.py
@@ -12,6 +12,7 @@ class Client(models.Model):
     designation = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
 
     class Meta:
         ordering = ['-created_at']
